1ef9145268fb73e569dac65edcf4ad929198cf86
Noraphat <punpunping@gmail.com>
2024-03-28 17:03:29 +0000

    added search bar (works)

--- js/index.js
+++ js/index.js
@@ -1,19 +1,58 @@
 let availableKeys = [
-    {songName: "", artistName: "ABBA", albumName: "", type: "artist"},
-    {songName: "", artistName: "ABBA", albumName: "Super Trouper", type: "artist"},
-    {songName: "Super Trouper", artistName: "ABBA", albumName: "Super Trouper", type: "song"}
+    {songName: "", artistName: "ABBA", albumName: "", type: "artist",  link: "artist.html"},
+    {songName: "", artistName: "ABBA", albumName: "Super Trouper", type: "album", link: "album.html"},
+    {songName: "Super Trouper", artistName: "ABBA", albumName: "Super Trouper", type: "song", link: "song.html"},
+    {songName: "B", artistName: "B", albumName: "B", type: "song", link: "song.html"},
 ]
 
-const navSearchResults = document.querySelector('nav-search-results');
-const inputBox = document.querySelector('.input-box');
+const navSearchResults = document.querySelector('.nav-search-results');
+const inputBox = document.getElementById('.input-box');
 
-inputBox.onkeyup = function(){
-    let result = [];
-    let input = inputBox.value.toLowerCase();
-    if (input.length) {
-        result = availableKeys.filter((key) => {
-            return key.artistName.toLowerCase().includes(input) || key.albumName.toLowerCase().includes(input) || key.songName.toLowerCase().includes(input);
-        });
-        console.log(result);
+document.addEventListener('DOMContentLoaded', (event) => {
+    const inputBox = document.getElementById('input-box');
+
+    function clearResults() {
+        navSearchResults.innerHTML = '';
     }
+
+
+    inputBox.onkeyup = function(){
+        let result = [];
+        let input = inputBox.value.toLowerCase();
+        if (input.length) {
+            // only check by type e.g. if type is artist then only check artist name
+            availableKeys.forEach((key) => {
+                if (key.type === 'artist' && key.artistName.toLowerCase().includes(input)) {
+                    result.push(key);
+                } else if (key.type === 'album' && key.albumName.toLowerCase().includes(input)) {
+                    result.push(key);
+                } else if (key.type === 'song' && key.songName.toLowerCase().includes(input)) {
+                    result.push(key);
+                }
+            });    
+        console.log(result)
+        navSearchResults.innerHTML = '';
+        dispalyResults(result);
+
+    } else {
+        clearResults();
+    }
+    
+}});
+
+
+function dispalyResults (result) {
+    navSearchResults.innerHTML = '';
+    result.forEach((key) => {
+        let a = document.createElement('a');
+        a.href = key.link;
+        if (key.type === 'artist') {
+            a.innerHTML = `Artist: ${key.artistName}`;
+        } else if (key.type === 'album') {
+            a.innerHTML = `Album: ${key.albumName}`;
+        } else {
+            a.innerHTML = `Song: ${key.songName}`;
+        }
+        navSearchResults.appendChild(a);
+    });
 }
