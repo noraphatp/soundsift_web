d4ae49e2988222cb6ec805a09b0745f784385481
Noraphat <punpunping@gmail.com>
2024-03-29 14:31:05 +0000

    check

--- js/find_songs.js
+++ js/find_songs.js
@@ -1,4 +1,65 @@
-// can add corresponding image and such later on
+let availableKeys = [
+  {songName: "", artistName: "ABBA", albumName: "", type: "artist",  link: "artist.html"},
+  {songName: "", artistName: "ABBA", albumName: "Super Trouper", type: "album", link: "album.html"},
+  {songName: "Super Trouper", artistName: "ABBA", albumName: "Super Trouper", type: "song", link: "song.html"},
+  {songName: "B", artistName: "B", albumName: "B", type: "song", link: "song.html"},
+]
+
+const navSearchResults = document.querySelector('.nav-search-results');
+const inputBox = document.getElementById('.input-box');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const inputBox = document.getElementById('input-box');
+
+  function clearResults() {
+      navSearchResults.innerHTML = '';
+  }
+
+
+  inputBox.onkeyup = function(){
+      let result = [];
+      let input = inputBox.value.toLowerCase();
+      if (input.length) {
+          // only check by type e.g. if type is artist then only check artist name
+          availableKeys.forEach((key) => {
+              if (key.type === 'artist' && key.artistName.toLowerCase().includes(input)) {
+                  result.push(key);
+              } else if (key.type === 'album' && key.albumName.toLowerCase().includes(input)) {
+                  result.push(key);
+              } else if (key.type === 'song' && key.songName.toLowerCase().includes(input)) {
+                  result.push(key);
+              }
+          });    
+      console.log(result)
+      navSearchResults.innerHTML = '';
+      dispalyResults(result);
+
+  } else {
+      clearResults();
+  }
+  
+}});
+
+
+function dispalyResults (result) {
+  navSearchResults.innerHTML = '';
+  result.forEach((key) => {
+      let a = document.createElement('a');
+      a.href = key.link;
+      if (key.type === 'artist') {
+          a.innerHTML = `Artist: ${key.artistName}`;
+      } else if (key.type === 'album') {
+          a.innerHTML = `Album: ${key.albumName}`;
+      } else {
+          a.innerHTML = `Song: ${key.songName}`;
+      }
+      navSearchResults.appendChild(a);
+  });
+}
+
+// ---------------------------- //
+
+// search songs functionality
 const songs = [
   // add image cover
   { name: "Song 1", artist: "Artist A", similarity: 0.93, duration: "2:00", image: "/song_covers/acoustic_1.jpg", album: "Album 1" },
@@ -221,3 +282,5 @@ slider.oninput = function () {
 function updateMoodValue(value, outputId) {
   document.getElementById(outputId).textContent = value;
 }
+
+
