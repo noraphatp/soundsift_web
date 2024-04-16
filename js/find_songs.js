e668a5493fea217bd806a3613cbe5d1f2e5ff2d7
Punpun Pingsuthiwong <np818@bath.ac.uk>
2024-04-17 00:40:48 +0100

    searchbar in sift search has added functionality, fixed musical octagon

--- js/find_songs.js
+++ js/find_songs.js
@@ -1,66 +1,69 @@
-let availableKeys = [
-  {songName: "", artistName: "ABBA", albumName: "", type: "artist",  link: "ABBA.html"},
-  {songName: "", artistName: "ABBA", albumName: "Super Trouper", type: "album", link: "album.html"},
-  {songName: "Super Trouper", artistName: "ABBA", albumName: "Super Trouper", type: "song", link: "song.html"},
-  {songName: "B", artistName: "B", albumName: "B", type: "song", link: "song.html"},
-]
+const availableKeys = [
+  { songName: "", artistName: "ABBA", albumName: "", type: "artist", link: "ABBA.html" },
+  { songName: "", artistName: "ABBA", albumName: "Super Trouper", type: "album", link: "album.html" },
+  { songName: "Super Trouper", artistName: "ABBA", albumName: "Super Trouper", type: "song", link: "song.html" },
+  { songName: "B", artistName: "B", albumName: "B", type: "song", link: "song.html" },
+];
 
-const navSearchResults = document.querySelector('.nav-search-results');
-const inputBox = document.getElementById('.input-box');
+const navSearchResults = document.querySelector(".nav-search-results");
+const inputBox = document.getElementById(".input-box");
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  const inputBox = document.getElementById('input-box');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const inputBox = document.getElementById("input-box");
 
   function clearResults() {
-      navSearchResults.innerHTML = '';
+    navSearchResults.innerHTML = "";
   }
 
-
-  inputBox.onkeyup = function(){
-      let result = [];
-      let input = inputBox.value.toLowerCase();
-      if (input.length) {
-          // only check by type e.g. if type is artist then only check artist name
-          availableKeys.forEach((key) => {
-              if (key.type === 'artist' && key.artistName.toLowerCase().includes(input)) {
-                  result.push(key);
-              } else if (key.type === 'album' && key.albumName.toLowerCase().includes(input)) {
-                  result.push(key);
-              } else if (key.type === 'song' && key.songName.toLowerCase().includes(input)) {
-                  result.push(key);
-              }
-          });    
-      console.log(result)
-      navSearchResults.innerHTML = '';
-      dispalyResults(result);
-
-  } else {
+  inputBox.onkeyup = function () {
+    let result = [];
+    let input = inputBox.value.toLowerCase();
+    if (input.length) {
+      // only check by type e.g. if type is artist then only check artist name
+      availableKeys.forEach((key) => {
+        if (key.type === "artist" && key.artistName.toLowerCase().includes(input)) {
+          result.push(key);
+        } else if (key.type === "album" && key.albumName.toLowerCase().includes(input)) {
+          result.push(key);
+        } else if (key.type === "song" && key.songName.toLowerCase().includes(input)) {
+          result.push(key);
+        }
+      });
+      console.log(result);
+      navSearchResults.innerHTML = "";
+      displayResults(result);
+    } else {
       clearResults();
-  }
-  
-}});
+    }
+  };
+});
 
-
-function dispalyResults (result) {
-  navSearchResults.innerHTML = '';
+function displayResults(result) {
+  navSearchResults.innerHTML = "";
   result.forEach((key) => {
-      let a = document.createElement('a');
-      // this is where the link will go, change the array as needed
-      a.href = key.link;
-      if (key.type === 'artist') {
-          a.innerHTML = `Artist: ${key.artistName}`;
-      } else if (key.type === 'album') {
-          a.innerHTML = `Album: ${key.albumName}`;
-      } else {
-          a.innerHTML = `Song: ${key.songName}`;
-      }
-      navSearchResults.appendChild(a);
+    let a = document.createElement("a");
+    // this is where the link will go, change the array as needed
+    a.href = key.link;
+    if (key.type === "artist") {
+      a.innerHTML = `Artist: ${key.artistName}`;
+    } else if (key.type === "album") {
+      a.innerHTML = `Album: ${key.albumName}`;
+    } else {
+      a.innerHTML = `Song: ${key.songName}`;
+    }
+    navSearchResults.appendChild(a);
   });
 }
 
 // ---------------------------- //
 
-// search songs functionality
+// This will be the song keys that will be used to search for songs
+const song_keys = [
+  { name: "Super Trouper", artist: "ABBA" },
+  { name: "Back in Black", artist: "ACDC" },
+];
+
+// results of the search
 const songs = [
   // add image cover
   { name: "Song 1", artist: "Artist A", similarity: 0.93, duration: "2:00", image: "./demo_song_covers/acoustic_1.jpg", album: "Album 1" },
@@ -69,7 +72,65 @@ const songs = [
   { name: "Song 4", artist: "Artist D", similarity: 0.85, duration: "2:33", image: "./demo_song_covers/night_street_2.jpg", album: "Album 2" },
 ];
 
+// Function to search for songs
+document.addEventListener("DOMContentLoaded", function () {
+  const searchBar = document.getElementById("searchBar");
+
+  searchBar.addEventListener("input", function () {
+    const searchQuery = searchBar.value.trim();
+    const dropdown = document.getElementById("searchDropdown");
+
+    // evwent listener for when user types in the search bar, if empty hide dropdown
+
+    // Function to filter songs based on input and update dropdown
+    const updateDropdown = () => {
+      const searchQuery = searchBar.value.trim().toLowerCase();
+      const filteredSongs = song_keys.filter((song) => {
+        return song.name.toLowerCase().includes(searchQuery) || song.artist.toLowerCase().includes(searchQuery);
+      });
+
+      if (!searchQuery) {
+        dropdown.style.display = "none";
+        return;
+      }
+
+      // Clear current dropdown contents
+      dropdown.innerHTML = "";
+
+      // Create new dropdown items from filtered songs
+      filteredSongs.forEach((song) => {
+        const dropdownItem = document.createElement("div");
+        dropdownItem.textContent = `${song.name} by ${song.artist}`;
+        dropdownItem.classList.add("dropdown-item");
+        dropdownItem.onclick = () => {
+          searchBar.value = song.name; // set search bar text to the song name
+          searchSong(); // call the search function
+          dropdown.style.display = "none"; // hide the dropdown
+        };
+        dropdown.appendChild(dropdownItem);
+      });
+
+      // Show dropdown if there are results
+      dropdown.style.display = filteredSongs.length > 0 ? "flex" : "none";
+    };
+
+    // Event listener for input on search bar
+    searchBar.addEventListener("input", updateDropdown);
+
+    // Event listener for clicking outside of the dropdown to hide it
+    // document.addEventListener("click", (event) => {
+    //   if (!dropdown.contains(event.target) && !searchBar.contains(event.target)) {
+    //     dropdown.style.display = "none";
+    //   }
+    // });
+  });
+});
+
 function searchSong() {
+  const dropdown = document.getElementById("searchDropdown");
+  // clear dropdown
+  dropdown.innerHTML = "";
+
   const searchQuery = document.getElementById("searchBar").value.trim();
   const resultsContainer = document.getElementById("results");
   resultsContainer.innerHTML = "";
@@ -137,15 +198,33 @@ function searchSong() {
 
       // Thumbs up and down buttons wrapped inside a div tag
       const thumbsUpDownDiv = document.createElement("div");
-      thumbsUpDownDiv.classList.add("thumbs-up-down");
-      
+      thumbsUpDownDiv.classList.add("thumbs-up-down-container");
 
+      const thumbsUpButton = document.createElement("button");
+      thumbsUpButton.classList.add("thumbs-button");
+      thumbsUpButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+        </svg>
+      `;
+
+      const thumbsDownButton = document.createElement("button");
+      thumbsDownButton.classList.add("thumbs-button");
+      thumbsDownButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M7.498 15.25H4.372c-1.026 0-1.945-.694-2.054-1.715a12.137 12.137 0 0 1-.068-1.285c0-2.848.992-5.464 2.649-7.521C5.287 4.247 5.886 4 6.504 4h4.016a4.5 4.5 0 0 1 1.423.23l3.114 1.04a4.5 4.5 0 0 0 1.423.23h1.294M7.498 15.25c.618 0 .991.724.725 1.282A7.471 7.471 0 0 0 7.5 19.75 2.25 2.25 0 0 0 9.75 22a.75.75 0 0 0 .75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 0 0 2.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384m-10.253 1.5H9.7m8.075-9.75c.01.05.027.1.05.148.593 1.2.925 2.55.925 3.977 0 1.487-.36 2.89-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398-.306.774-1.086 1.227-1.918 1.227h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 0 0 .303-.54" />
+        </svg>
+      `;
+
+      thumbsUpDownDiv.appendChild(thumbsUpButton);
+      thumbsUpDownDiv.appendChild(thumbsDownButton);
+      songDiv.appendChild(thumbsUpDownDiv);
 
       const dropdownButton = document.createElement("button");
       dropdownButton.classList.add("dropdown-button");
       dropdownButton.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
         </svg>
       `;
 
@@ -289,5 +368,3 @@ slider.oninput = function () {
 function updateMoodValue(value, outputId) {
   document.getElementById(outputId).textContent = value;
 }
-
-
